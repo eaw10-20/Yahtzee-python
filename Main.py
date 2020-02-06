ba45d0072e92645e275e3c6f4614340199fc7b8b
eaw10-20 <60686880+eaw10-20@users.noreply.github.com>
2020-02-05 22:13:12 -0800

    Feature update
    implemented bonus score
    eliminating previously chosen portions of scorecard

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#note to self: need to make a class and object to store all of these variables
+#note to self: need a switch for first yahtzee (so you can't just select it in the beginning)
 import random       #used for random number generation
 
 
@@ -9,7 +9,9 @@ class Game:
         self.holds = [False, False, False, False, False]    # marks which dice are being held
         self.chances = 3                                    # number of times to roll dice
         self.table = []
-        self.switch = False                                 #to indicate if the score has changed
+        self.switch = False                                 # to indicate if the score has changed
+        self.bonus = 0                                      # bonus points
+        self.gotbonus = False                               # indicates whether or not you got bonus already
         for i in range(13):
             self.table.append(False)
 
@@ -132,7 +134,7 @@ def yahtzee(dice):
     return 50
 
 
-def displayscore(dice):
+def displayscore(stat):
     #----------------
     #render scorecard
     #----------------
@@ -140,19 +142,24 @@ def displayscore(dice):
     scorecard = []
     # upper section
     for i in range(6):
-        scorecard.append(get_upper(dice, i + 1))        # 0-5
+        scorecard.append(get_upper(stat.dice, i + 1))        # 0-5
     # 3 and 4 of a kind
-    scorecard.append(of_a_kind(dice, 3))                # 6
-    scorecard.append(of_a_kind(dice, 4))                # 7
+    scorecard.append(of_a_kind(stat.dice, 3))                # 6
+    scorecard.append(of_a_kind(stat.dice, 4))                # 7
     # full house
-    scorecard.append(full_house(dice))                  # 8
+    scorecard.append(full_house(stat.dice))                  # 8
     # small and large straight
-    scorecard.append(straight(dice, 4))                 # 9
-    scorecard.append(straight(dice, 5))                 # 10
+    scorecard.append(straight(stat.dice, 4))                 # 9
+    scorecard.append(straight(stat.dice, 5))                 # 10
     # YAHTZEE!
-    scorecard.append(yahtzee(dice))                     # 11
+    scorecard.append(yahtzee(stat.dice))                     # 11
     # chance
-    scorecard.append(of_a_kind(dice, 0))                # 12
+    scorecard.append(of_a_kind(stat.dice, 0))                # 12
+
+    # remove sections already chosen
+    for i, check in enumerate(stat.table):
+        if check:
+            scorecard[i] = 'X'
 
     # --------------
     # show scorecard
@@ -165,7 +172,7 @@ def displayscore(dice):
     print(f"[4] 4's:    {scorecard[3]}      [10] small straight: {scorecard[9]}")
     print(f"[5] 5's:    {scorecard[4]}      [11] large straight: {scorecard[10]}")
     print(f"[6] 6's:    {scorecard[5]}      [12] yahtzee!:       {scorecard[11]}")
-    print(f"bonus score - 0    [13] chance: {scorecard[12]}")
+    print(f"bonus score - {stat.bonus}    [13] chance: {scorecard[12]}")
     return scorecard
 
 
@@ -187,7 +194,7 @@ def addscore(stat, skippable):
         13 : "chance"
     }
     while loop:
-        scoretable = displayscore(stat.dice)
+        scoretable = displayscore(stat)
         try:
             if skippable:
                 val = int(input("What would you like to use? Enter 0 to return: "))
@@ -197,9 +204,12 @@ def addscore(stat, skippable):
             print("Invalid input\n")
             continue
         if 0 < val <= 13:
-            print(f"Are you sure you would like to use your {name[val]} (y/n)?")
-            yn = input()
-            loop = not y_or_n(yn)
+            if scoretable[val - 1] != 'X':
+                print(f"Are you sure you would like to use your {name[val]} (y/n)?")
+                yn = input()
+                loop = not y_or_n(yn)
+            else:
+                print(f"You've already used your {name[val]}.")
         elif val == 0 and skippable:
             loop = False
         else:
@@ -208,6 +218,12 @@ def addscore(stat, skippable):
         stat.switch = True
         stat.table[val - 1] = True
         stat.score += scoretable[val - 1]
+        # bonus score
+        if val <= 6:
+            stat.bonus += scoretable[val - 1]
+            if stat.bonus >= 63 and not stat.gotbonus:
+                stat.score += 35
+                stat.gotbonus = True
     return stat
 
 
